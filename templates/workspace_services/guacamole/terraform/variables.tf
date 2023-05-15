373f0f3795575d939a3a4490a337c8d2f82d3fe9
Saurabh Rajio Mahajan <v-smahaj@microsoft.com>
2023-05-15 14:38:11 -0400

    Added Azure ML workspace service

---
+++ templates/workspace_services/guacamole/terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "workspace_id" {}
+variable "aad_authority_url" {}
+variable "tre_id" {}
+variable "mgmt_resource_group_name" {}
+variable "mgmt_acr_name" {}
+variable "image_name" {}
+variable "image_tag" {}
+variable "guac_disable_copy" {}
+variable "guac_disable_paste" {}
+variable "guac_enable_drive" {}
+variable "guac_drive_name" {}
+variable "guac_drive_path" {}
+variable "guac_disable_download" {}
+variable "guac_disable_upload" {}
+variable "is_exposed_externally" {}
+variable "tre_resource_id" {}
+variable "arm_environment" {}
